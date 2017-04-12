3bfc9dd41a46d821d4b3d3acb90c30c0955bcdbb
M558758 <scott.le@macys.com>
2017-04-11 23:37:19 -0700

    second commit

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/m558758/apps/mysite/env"
+set -gx VIRTUAL_ENV "/Users/m558758/apps/mysite/ENV"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
